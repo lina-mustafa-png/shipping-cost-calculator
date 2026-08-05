42875ff4d3baf293fdef177ee8088ddcd6cd4061
lina-mustafa-png <lina9.mustafa90@gmail.com>
2026-08-05 09:29:53 -0400

    Update ExpressParcel.java

--- ExpressParcel.java
+++ ExpressParcel.java
@@ -3,7 +3,6 @@ class ExpressParcel extends StandardParcel {
 
     public ExpressParcel(double weightKg, double distanceKm, boolean insured,
                          String sizeTier, boolean sameDay) {
-       //TO DO: Complete the constructor
        super(weightKg, distanceKm, insured, sizeTier);
        this.sameDay = sameDay;
        
@@ -15,7 +14,6 @@ class ExpressParcel extends StandardParcel {
         double cost = super.shippingCost();
         // Express surcharge: faster delivery multiplier + optional same-day fee
         cost *= 1.35; // express speed premium
-        //TO DO update cost based on sameDay
         if (sameDay == true){
             cost += 7.50;
         }
